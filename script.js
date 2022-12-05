58252100a7fa25989c78bd8f3c0af1ed75c6ecb6
Skraevern <kristian.skreosen@icloud.com>
2022-12-05 10:09:02 +0100

    Added show read unread or all function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let newBook;
 
 let myLibrary = [];
-let displayMyLibraryArr = myLibrary;
+let myLibraryDisplayArr = myLibrary;
 let libraryList = document.getElementById("book-list");
 let libraryOverview = document.getElementById("library-overview");
 let form = document.getElementById("form");
@@ -44,55 +44,39 @@ function addBookToLibrary() {
 
 function displayLibraryCards() {
     libraryList.innerHTML = "";
-    console.log(showBy.value);
-    switch (showBy.value) {
-        case "all":
-            displayMyLibraryArr = myLibrary;
-            break;
-        case "read":
-            for (let i = 0; i < myLibrary.length; i++) {
-                if (myLibrary[i].read === "read") {
-                    displayMyLibraryArr.push(myLibrary[i]);
-                }
-            }
-            break;
-        case "unread":
-            for (let i = 0; i < myLibrary.length; i++) {
-                if (myLibrary[i].read === "unread") {
-                    displayMyLibraryArr.push(myLibrary[i]);
-                }
-            }
-    }
-    console.log(displayMyLibraryArr);
-    for (let i = 0; i < displayMyLibraryArr.length; i++) {
+    myLibraryDisplayArr = [];
+    showReadUnread();
+
+
+    for (let i = 0; i < myLibraryDisplayArr.length; i++) {
         let div = document.createElement("div");
         div.classList.add("list-card");
         div.id = `div${i}`;
 
-        for (const key in displayMyLibraryArr[i]) {
+        for (const key in myLibraryDisplayArr[i]) {
             let value = document.createElement("p");
             switch (key) {
                 case "title":
-                    value.textContent = displayMyLibraryArr[i][key];
+                    value.textContent = myLibraryDisplayArr[i][key];
                     value.style.fontWeight = "bold";
                     break;
                 case "author":
-                    value.textContent = `By: ${displayMyLibraryArr[i][key]}`;
+                    value.textContent = `By: ${myLibraryDisplayArr[i][key]}`;
                     break;
                 case "read":
-                    displayMyLibraryArr[i][key] === "read"
+                    myLibraryDisplayArr[i][key] === "read"
                         ? (value.textContent = "Book read")
                         : (value.textContent = "Book not read");
                     break;
                 case "pages":
-                    value.textContent = `Number of Pages: ${displayMyLibraryArr[i][key]}`;
+                    value.textContent = `Number of Pages: ${myLibraryDisplayArr[i][key]}`;
                     break;
                 case "published":
-                    value.textContent = `Published: ${displayMyLibraryArr[i][key]}`;
+                    value.textContent = `Published: ${myLibraryDisplayArr[i][key]}`;
                     break;
 
                 case "description":
-                    value.textContent = displayMyLibraryArr[i][key];
+                    value.textContent = myLibraryDisplayArr[i][key];
             }
             div.appendChild(value);
         }
@@ -103,7 +87,26 @@ function displayLibraryCards() {
         libraryList.appendChild(div);
     }
 }
-
+function showReadUnread() {
+    switch (showBy.value) {
+        case "all":
+            myLibraryDisplayArr = myLibrary;
+            break;
+        case "read":
+            for (let i = 0; i < myLibrary.length; i++) {
+                if (myLibrary[i].read === "read") {
+                    myLibraryDisplayArr.push(myLibrary[i]);
+                }
+            }
+            break;
+        case "unread":
+            for (let i = 0; i < myLibrary.length; i++) {
+                if (myLibrary[i].read === "unread") {
+                    myLibraryDisplayArr.push(myLibrary[i]);
+                }
+            }
+    } 
+};
 clearForm = function () {
     document.getElementById("title").value = "";
     document.getElementById("author").value = "";
@@ -132,7 +135,6 @@ function updateLibraryOverview() {
     totalUnreadBooks = 0;
 
     for (let i = 0; i < myLibrary.length; i++) {
-        console.log(myLibrary[i].read);
         if (myLibrary[i].read === "read") {
             totalReadBooks++;
         }
